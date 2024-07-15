e01ce2d4ac41185a0239687711d9a58bf0bb0769
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-07-15 17:18:24 +0500

    chapter 2 added

---
+++ Chap-2/airthmeticOperataors.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+int a = 10, b = 20;
+int c  = a + b;
+int d = a - b;
+int e = a * b;
+int f = a / b;
+int g = a % b;
+printf("Sum of a and b is %d\n", c);
+printf("Difference of a and b is %d\n", d);
+printf("Product of a and b is %d\n", e);
+printf("Division of a and b is %d\n", f);
+printf("Modulus of a and b is %d\n", g);
+return 0;
+}
